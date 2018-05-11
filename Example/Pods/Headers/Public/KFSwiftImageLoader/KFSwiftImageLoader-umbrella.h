7b2b3307f34850f63d5349ede5407ccf375559ac
Kiavash Faisali <kiavashfaisali@outlook.com>
2018-05-11 17:38:18 -0400

    Ran fresh pod install with CocoaPods 1.5.2, and fixed some errors.

--- Example/Pods/Headers/Public/KFSwiftImageLoader/KFSwiftImageLoader-umbrella.h
+++ Example/Pods/Headers/Public/KFSwiftImageLoader/KFSwiftImageLoader-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_KFSwiftImageLoader_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_KFSwiftImageLoader_ExampleVersionString[];
+FOUNDATION_EXPORT double KFSwiftImageLoaderVersionNumber;
+FOUNDATION_EXPORT const unsigned char KFSwiftImageLoaderVersionString[];
 
--- Example/Pods/Target Support Files/KFSwiftImageLoader/KFSwiftImageLoader-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double KFSwiftImageLoaderVersionNumber;
-FOUNDATION_EXPORT const unsigned char KFSwiftImageLoaderVersionString[];
-
